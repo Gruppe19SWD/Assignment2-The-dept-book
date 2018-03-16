ed55a982aec479f247cd2ba7b7b2897fec887f4e
Morten <mo.rt.en@hotmail.com>
2018-03-16 10:51:43 +0100

    Added more persons/loans

--- OverviewWindow/Model/DeptModel.cs
+++ OverviewWindow/Model/DeptModel.cs
@@ -6,13 +6,20 @@ namespace OverviewWindow.Model
     {
         private List<Person> persons = new List<Person>();
         private List<Loan> loan1 = new List<Loan>();
+        private List<Loan> loan2 = new List<Loan>();
+        private List<Loan> loan3 = new List<Loan>();
 
         public DeptModel()
         {
-            loan1.Add(new Loan("Købte kaffe", -9));
-            persons.Add(new Person("Anders",loan1));
-            
-        }
+            loan1.Add(new Loan("Kaffe", -9));
+            persons.Add(new Person("Anders", loan1));
+            loan1.Add(new Loan("Kage", -12));
+            loan1.Add(new Loan("Pizza", +35));
+            persons.Add(new Person("Morten", loan2));
+            loan2.Add(new Loan("Flybillet til Maldiverne", -4979));
+            loan2.Add(new Loan("Dykkertur", -1989));
+            persons.Add(new Person("Udbetaling Danmark", loan3));
+            loan3.Add(new Loan("Kontigent", -75));
 
         public List<Person> GetPersons()
         {
